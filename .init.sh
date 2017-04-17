d332971939009e939df14c79061fbe90e937b80e
Inom Turdikulov <i@inomoz.ru>
2017-04-18 02:22:52 +0300

    better boilerplate generation

--- .init.sh
+++ .init.sh
@@ -2,11 +2,32 @@
 
 cd "$(dirname "$0")"
 
-svn export https://github.com/h5bp/html5-boilerplate/trunk/dist src
-cd src
-rm .editorconfig .gitattributes .gitignore .htaccess
-rm -r css
-rm -r js/vendor
-mkdir sass
-mkdir img/icons
-touch sass/main.sass
+function init_boilerplate {
+  svn export https://github.com/h5bp/html5-boilerplate/trunk/dist src
+  cd src
+  rm .editorconfig .gitattributes .gitignore .htaccess
+  rm -r css
+  rm -r js/vendor
+  mkdir sass
+  mkdir img/icons
+  touch sass/main.sass
+}
+
+if [ -d "src" ]; then
+  read -p "Remove src directory and download boilerplate?" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    rm -rf src/
+    init_boilerplate
+  fi
+else
+  init_boilerplate
+fi
+
+read -p "Install bulma NPM module? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    npm install bulma --save
+fi
